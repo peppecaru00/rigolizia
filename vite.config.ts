a054d065f8925542b8e572359fe78d712dbdd2ef
peppecaru00 <carusothe99@gmail.com>
2026-04-01 09:14:31 +0200

    fixed path

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/rigolizia/',
+  base: '/',
   plugins: [
     react(),
     tailwindcss(),
